8a3b180d6e5aaa069ab20e1de61aebf8304d37f2
Analyst <analyst@Analysts-MacBook-Pro.local>
2018-01-31 18:00:31 -0500

    config skeleton done

---
+++ config.go
@@ -0,0 +1,37 @@
+package main
+
+type Config struct {
+	winTools WindowsTools
+	nixTools LinuxTools
+	macTools OSXTools
+}
+
+type WindowsTools struct {
+	bulk_extractor bool
+	fiwalk bool
+	frag_find bool
+	tcpflow bool
+	afxml bool
+	SleuthKit bool
+	MFTDump bool
+	WinPrefetch bool
+	RegRipper bool
+}
+
+type LinuxTools struct {
+	ps bool
+	mrutools bool
+	LinuxForensicsTool bool
+	BulkExtractor bool
+	ENT bool
+	Fdupes bool
+	Foremost bool
+}
+
+type OSXTools struct {
+	OSXAudiotr bool
+	KnockKnock bool
+	Pac4Mac bool
+	KeychainForensicTool bool
+	OSXCollector bool
+}
--- main.go
+++ main.go
@@ -4,28 +4,41 @@ import (
 	"fmt"
 	"flag"
 	"io/ioutil"
+	"encoding/json"
 )
 
     
 
 func main() {
 
-	configPtr := flag.String("config", "config.txt", "Loction of the configuration file")
+	configPathPtr := flag.String("config", "config.txt", "Loction of the configuration file")
 	flag.Parse()
-	parseConfig(*configPtr)
+	config Config = parseConfig(*configPathPtr) // returns config
+	
+	if runtime.GOOS == "windows" {
+		windowsTools(config)
+	} //else if untime.GOOS == 
 
 }
 
 
-func parseConfig(configFile string) {
+func parseConfig(configFile string) Config {
 
 	b, err := ioutil.ReadFile(configFile) 
     if err != nil {
         fmt.Println("Unable to open config file: ", err)
     }
 
-    fmt.Println(string(b))
+    myConfig := Config{}
 
+    if err := json.Unmarshal(b, &myConfig); err != nil {
+        fmt.Println("Error!\n", err)
+    }
 
+    return myConfig
+
+}
+
+func windowsTools() {
 
 }
