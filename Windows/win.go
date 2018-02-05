d6bba66d71bdde02d94b55089216bc126055d8a9
Analyst <bernieram@appstate.edu>
2018-02-04 20:08:49 -0500

    Parsed the scanned buffer for bulkextractor progress

--- Windows/win.go
+++ Windows/win.go
@@ -3,29 +3,41 @@ package Windows
 import (
 	"os/exec"
 	"fmt"
-	"bytes"
 	"regexp"
+	"bufio"
+	"strings"
 )
 func BulkExtractor(args string) {
-	fmt.Println(args)
-	myArgs := []string{"/C", "Windows\\bulk_extractor32.exe"}
 
+	myArgs := []string{"/C", "Windows\\bulk_extractor32.exe"}
 	r := regexp.MustCompile("[^\\s]+")
 	myArgs2 := r.FindAllString(args, -1)
-
 	myArgs = append(myArgs, myArgs2...)
-	//cmd := exec.Command("cmd", "/C", "Windows\\bulk_extractor32.exe", strings.Fields(args))
+
 	cmd := exec.Command("cmd", myArgs...)
 
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		return
+	//var out bytes.Buffer
+	//var stderr bytes.Buffer
+	//cmd.Stdout = &out
+	//cmd.Stderr = &stderr
+	//err := cmd.Run()
+	//if err != nil {
+	//	fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
+	//	return
+	//}
+	//fmt.Println("Result: " + out.String())
+
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		m := scanner.Text()
+		if strings.Contains(m, "(") {
+			fmt.Println("BulkExtractor: " + m[strings.Index(m, "(") + 1:strings.Index(m,")")] + "% Complete")
+		}
 	}
-	fmt.Println("Result: " + out.String())
+
+	cmd.Wait()
 
 }
