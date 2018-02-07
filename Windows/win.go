ef76ad65cd5dd757f261cdf22cf95bd14bfcadf1
Analyst <bernieram@appstate.edu>
2018-02-07 17:16:43 -0500

    added sleuthkit

--- Windows/win.go
+++ Windows/win.go
@@ -13,7 +13,7 @@ import (
 
 func BulkExtractor(args string) {
 
-	cmd :=  runTool(args, "bulk_extractor32.exe")
+	cmd :=  cmdTool(args, "bulk_extractor32.exe")
 	stdout, _ := cmd.StdoutPipe()
 	cmd.Start()
 
@@ -21,7 +21,7 @@ func BulkExtractor(args string) {
 	for scanner.Scan() {
 		m := scanner.Text()
 		if strings.Contains(m, "(") {
-			fmt.Println("BulkExtractor: " + m[strings.Index(m, "(") + 1:strings.Index(m,")")] + "% Complete")
+			fmt.Println("BulkExtractor: " + m[strings.Index(m, "(") + 1:strings.Index(m,")")] + " Complete")
 		}
 	}
 
@@ -31,7 +31,130 @@ func BulkExtractor(args string) {
 }
 
 func Fiwalk(args string) {
-	cmd :=  runTool(args, "fiwalk-0.6.3.exe")
+	cmd :=  cmdTool(args, "fiwalk-0.6.3.exe")
+	runDefault(cmd)
+}
+
+func blkcalc(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\blkcalc.exe")
+	runDefault(cmd)
+}
+
+func blkcat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\blkcat.exe")
+	runDefault(cmd)
+}
+
+func blkstat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\blkstat.exe")
+	runDefault(cmd)
+}
+
+func fcat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\fcat.exe")
+	runDefault(cmd)
+}
+
+func ffind(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\ffind.exe")
+	runDefault(cmd)
+}
+
+func fls(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\fls.exe")
+	runDefault(cmd)
+}
+
+func fsstat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\fsstat.exe")
+	runDefault(cmd)
+}
+
+func hfind(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\hfind.exe")
+	runDefault(cmd)
+}
+
+func icat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\icat.exe")
+	runDefault(cmd)
+}
+
+func ifind(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\ifind.exe")
+	runDefault(cmd)
+}
+
+func ils(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\ils.exe")
+	runDefault(cmd)
+}
+
+func img_cat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\img_cat.exe")
+	runDefault(cmd)
+}
+
+func img_stat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\img_stat.exe")
+	runDefault(cmd)
+}
+
+func istat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\istat.exe")
+	runDefault(cmd)
+}
+
+func jcat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\jcat.exe")
+	runDefault(cmd)
+}
+
+func jls(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\jls.exe")
+	runDefault(cmd)
+}
+
+func mmcat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\mmcat.exe")
+	runDefault(cmd)
+}
+
+func mmls(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\mmls.exe")
+	runDefault(cmd)
+}
+
+func mmstat(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\mmstat.exe")
+	runDefault(cmd)
+}
+
+
+func tsk_comparedir(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\tsk_comparedir.exe")
+	runDefault(cmd)
+}
+
+func tsk_gettimes(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\tsk_gettimes.exe")
+	runDefault(cmd)
+}
+
+
+func tsk_loaddb(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\tsk_loaddb.exe")
+	runDefault(cmd)
+}
+
+
+func tsk_recover(args string) {
+	cmd :=  cmdTool(args, "sleuthkit\\bin\\tsk_recover.exe")
+	runDefault(cmd)
+}
+
+
+func runDefault(cmd *exec.Cmd) {
 	stdout, _ := cmd.StdoutPipe()
 	cmd.Start()
 
@@ -45,8 +168,7 @@ func Fiwalk(args string) {
 }
 
 
-
-func runTool(args string, tool string) *exec.Cmd {
+func cmdTool(args string, tool string) *exec.Cmd {
 	myArgs := []string{"/C", "Windows\\" + tool}
 	r := regexp.MustCompile("[^\\s]+")
 	myArgs2 := r.FindAllString(args, -1)
