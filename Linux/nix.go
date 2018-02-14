d04d86246a8c15e3be3dced64e8f6080d0aa3312
Analyst <bernieram@appstate.edu>
2018-02-14 17:20:58 -0500

    added sleuthkit

--- Linux/nix.go
+++ Linux/nix.go
@@ -8,3 +8,32 @@ import (
 	"regexp"
 )
 
+func Mrutools(args string) {
+	cmd :=  cmdTool(args, "fiwalk-0.6.3.exe")
+	runDefault(cmd)
+}
+
+func runDefault(cmd *exec.Cmd) {
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+
+	cmd.Wait()
+}
+
+
+func cmdTool(args string, tool string) *exec.Cmd {
+	myArgs := []string{"/C", "Tools\\" + tool}
+	r := regexp.MustCompile("[^\\s]+")
+	myArgs2 := r.FindAllString(args, -1)
+	myArgs = append(myArgs, myArgs2...)
+
+	cmd := exec.Command("cmd", myArgs...)
+
+	return cmd
+}
