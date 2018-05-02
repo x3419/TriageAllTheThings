f548be2f9d4c8a4ee89c0c1f3b90bfc73d131d13
Alex Bernier <alexbernier11@gmail.com>
2018-05-01 22:21:16 -0400

    Update config.go

--- Configuration/config.go
+++ Configuration/config.go
@@ -1,10 +1,13 @@
+// Package Configuration contains the things necessary for configuration.
 package Configuration
 
+// Config struct is the configuration object that is seralized.
 type Config struct {
 	Tool         []Tool
 	RelativePath bool
 }
 
+// Tool contains a given tools settings.
 type Tool struct {
 	Name    string
 	Enabled bool
