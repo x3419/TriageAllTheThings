ca476505f9c7c4b728526da1ba0f275b8af269ff
Alex Bernier <alexbernier11@gmail.com>
2018-05-01 22:11:38 -0400

    Update osutil.go

--- Osutil/osutil.go
+++ Osutil/osutil.go
@@ -4,8 +4,9 @@ import (
 	"Capstone/Configuration"
 )
 
-// every operating system will implement this to run tools and build UI its own way
-// right now OSX and GNU/Linux aren't implementing GUI because ProtonMail/ui doesn't actually work cross platform despite claims
+
+// ToolRunner - every operating system will implement this to run tools and build UI its own way.
+// Right now OSX and GNU/Linux aren't implementing GUI because ProtonMail/ui doesn't actually work cross platform despite claims.
 type ToolRunner interface {
 	MakeGUI(config Configuration.Config)
 }
