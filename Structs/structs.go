1eaaa565e4a42da53f3bacf20df112d9884cb047
Alex Bernier <alexbernier11@gmail.com>
2018-05-01 22:20:11 -0400

    Update structs.go

--- Structs/structs.go
+++ Structs/structs.go
@@ -1,3 +1,4 @@
+// Package Structs contains the structures needed in code.
 package Structs
 
 import "os/exec"
@@ -5,11 +6,13 @@ import (
 	"github.com/ProtonMail/ui"
 )
 
+// Result contains the result of a command executable.
 type Result struct {
 	Command     *exec.Cmd
 	CommandFunc func(cmd *exec.Cmd)
 }
 
+// UIComp contains the struct for the necessary GUI components.
 type UIComp struct {
 	Label *ui.Label
 	Output *ui.MultilineEntry
